91f23fe2e667b1b172478a0c01ae8ad5a9d236f5
Semen <senia-2010@mail.ru>
2022-03-09 18:21:53 +0300

    add personal card

--- src/components/CityCard/CityCard.js
+++ src/components/CityCard/CityCard.js
@@ -2,10 +2,13 @@ import './CityCard.css';
 
 function CityCard() {
   return (
-    <>      
-   <p>Это карточка города</p>
-   <button type="submit">Показать погоду</button>
-    </>
+    <section className="personal-card">
+      <div className="personal-card__wrapper">
+        <p className="personal-card__wheather">Mostly Sunny</p>
+        <p className="personal-card__temp">54&deg;</p>
+        <p className="personal-card__text">Today, <span className="personal-card__today">Mostly Sunny</span> currently. It’s <span className="personal-card__today">54&deg;</span> and the high will be <span className="personal-card__today">54&deg;</span>.</p>
+      </div>
+    </section>
   );
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -16,8 +16,13 @@ function Header(props) {
                 <Search />
             </div>
             <div className="header-day">
+                <div className="header-day__text-wrapper">
                 {props.arrow}
                 <p className="header-day__text">Today</p>
+                
+                </div>
+                {/* {props.arrow}
+                <p className="header-day__text">Today</p> */}
                 <p className="header-day__week">Wednesday</p>
                 <Link to="/adds" className="header-day__link">
                     <svg className="header-day__link-img" width="27" height="27" viewBox="0 0 27 27" fill="none" xmlns="http://www.w3.org/2000/svg">
