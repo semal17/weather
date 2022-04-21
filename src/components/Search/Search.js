2ee3e8cbd7ce3b79b602dae100f1c9435c1a53ef
Semen <senia-2010@mail.ru>
2022-04-21 18:15:21 +0300

    add navigate

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,5 +1,6 @@
 import { useRef } from 'react';
 import { debounce } from "lodash";
+import { useNavigate } from 'react-router-dom';
 
 import './Search.css';
 
@@ -8,10 +9,13 @@ import './Search.css';
 function Search({ classSearch, search, setSearch }) {
 
   let input = useRef();
+  const navigate = useNavigate();
 
 
   let searchCity = debounce(() => {
     setSearch(input.current.value);
+    
+    navigate('/city')
   }, 1500);
 
   console.log(search);
